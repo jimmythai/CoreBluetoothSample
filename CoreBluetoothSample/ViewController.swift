1f9b581b760ce1ce9ee3234f401125d1fa9d6765
thaiatchan <ym@www-biz.co>
2018-12-05 14:01:45 +0900

    Initial Commit

---
+++ CoreBluetoothSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreBluetoothSample
+//
+//  Created by Atsushi Yamamoto on 2018/12/05.
+//  Copyright © 2018 Atsushi Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
